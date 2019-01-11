593f0748e5803c826b53f7b991049895f8dd6697
Andrew Kemp <andrewtkemp@gmail.com>
2019-01-11 12:23:21 -0600

    Got Artist Page Route Working

--- routes/api/artists.js
+++ routes/api/artists.js
@@ -5,29 +5,25 @@ const router = express.Router();
 
 
 router.get("/:letter", (req, res)=>{
-//     let url = "http://www.plyrics.com/lyrics/haveheart/thesameson.html"
-// let band = "Have Heart"
+    let url = `http://www.plyrics.com/${req.params.letter}.html`
 
-// request(url, function (error, response, body) {
-//   console.log("Hello World")
-//   const $ = cheerio.load(body)
-//   let test = $(".content_lyr").remove("h3").remove("h4").remove("h1").remove("script")
-//   $(test).each(function(i, element) {
-//       let text = $(this).text();
-//       console.log(text);
-//       let stuff = text.replace(/\n/g,'').replace("                 Print     Email","").split("Thanks")
-//       let content = stuff[0].split("LYRICS")
-//       let band = content[0].replace("Print","").replace("Email","").trim()
-//       let title = "The Same Son"
-//       let lyrics = content[1].split(title)
-//       let urlSplit = url.split("/")
-//       console.log(urlSplit)
-//       console.log("Band: " + band);
-//       console.log("Lyrics: " + lyrics);
-//   })
-// });
-    console.log(req.params.letter);
-    res.send("Hello World")
+request(url, function (error, response, body) {
+  console.log("Hello World")
+  const $ = cheerio.load(body)
+  let artists = [];
+  $(".artistlist").each(function(i, element) {
+        let text = $(this).text();
+        let href = $(this).attr("href");
+        console.log(text);
+        console.log(href);
+        let artistInfo = {
+            name:text,
+            href:href
+        }
+        artists.push(artistInfo)
+  })
+    res.send(artists);
+});
 })
 
 
