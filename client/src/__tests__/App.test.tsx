82f4094dfb4c93527be8ba4cdb3ce584499a951c
Andrew Kemp <andrewtkemp@gmail.com>
2019-02-07 16:45:06 -0600

    updated tsconfig.json

--- client/src/__tests__/App.test.tsx
+++ client/src/__tests__/App.test.tsx
@@ -1,8 +1,7 @@
 import * as React from "react";
-
 import {App} from "../components";
-import { shallow } from 'enzyme';
 import {Link} from "react-router-dom";
+import { shallow } from 'enzyme';
 describe("<App/>",()=>{
   it('renders without crashing', ()=>{
     shallow(<App/>);
